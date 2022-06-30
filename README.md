95976fd5bee91ca682c4290a3a9b7e9911e26431
lnogueira1985 <99752776+lnogueira1985@users.noreply.github.com>
2022-06-30 14:42:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# movie-drEstranho
+ site Dr Estranho
---
+++ assets/js/menu.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('.menu-burger-container')
+
+const active = () => menu.classList.toggle('active')
+
+menu.addEventListener('click', active)
+
+
+// const menu = document.querySelector(".menu-burger-container")
+
+// const active = () => menu.classList.toggle('active')
+
+// menu.addEventListener('click', active)
